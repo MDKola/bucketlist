57b6d7e1c3915b9777a6df6b89f4a6347f203e09
kola denno <denokolas@gmail.com>
2017-07-19 16:02:16 +0300

    Modify and add functionality for the user management

--- classes/users.py
+++ classes/users.py
@@ -1,31 +1,64 @@
 class Users(object):
+
     def __init__(self):
         self.users = {}
-        self.user_is_logged_in = False
+        self.user_is_logged_in = None
     
     def register(self, name, email, password):
-        msg = None
-        user_exists = False
-        for registered_email in self.users.keys():
-            if email == registered_email:
-                user_exists = True
-                msg = "An email {} already exists.".format(str(email))
-                break
-        if not user_exists:
-            self.users[email] = {'name': name, 'password': password}
+        error_msg = None
 
-        if msg is not None:
-            return msg
+        if type(name) != str:
+            error_msg = "Name cannot contain numbers"
+
+        if len(password) < 6:
+            error_msg = 'Password length should be at least 6 characters'
+
+        if len(email) < 0:
+            error_msg = 'Please provide an email address.'
+
+        if error_msg is None:
+            user_exists = False
+            for reg_email in self.users.keys():
+                if email == reg_email:
+                    user_exists = True
+                    error_msg = 'Email already exists.'
+                    break
+
+            if not user_exists:
+                #create account
+                self.users[email] = {'password': password, 'name': name}
+
+        if error_msg is not None:
+            return error_msg
     
-    def login(self, email, password ):
-        if registered_email in self.users.keys():
-            if email == registered_email:
-                registered_user = self.users[email]
-                registered_password = registered_user['password']
-                if password == registered_password:
-                    self.user_is_logged_in = email
-                else:
-                    msg = "Invalid password"
+    def login(self, email, password):
+        error_msg = None
 
-                break 
+        if len(password) == 0:
+            error_msg = 'Invalid password'
 
+        elif len(email) == 0:
+            error_msg = 'email field cannot be left empty'
+        
+        if error_msg is None:
+            user_in_record = False
+            for reg_email in self.users.keys():
+                if email == reg_email:
+                    user_in_record = True
+                    reg_user_details = self.users[email]
+                    reg_password = reg_user_details['password']
+
+                    if password == reg_password:
+                        self.user_is_logged_in = email
+                    else:
+                        error_msg = 'Incorect password'
+                    break
+
+            if not user_in_record:
+                error_msg = 'User does not exist.'
+                
+        if error_msg is not None:
+            return error_msg
+    
+    def logout(self):
+        self.user_is_logged_in = None
