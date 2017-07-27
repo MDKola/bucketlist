8c3f5b76c06c3ecf6bf3d275424ceb3522ffc446
kolaempire <denokolas@gmail.com>
2017-07-27 10:24:25 +0300

    Check pep8 and remove print statements

--- app.py
+++ app.py
@@ -12,210 +12,228 @@ app = Flask(__name__)
 bucketlists = {}
 users = Users()
 
+
 @app.route('/')
 def index():
-	if users.user_is_logged_in is not None:
-		return redirect('/dashboard')
-	else:	
-		return render_template('index.html')
+    """Display the welcome page"""
+    if users.user_is_logged_in is not None:
+        return redirect('/dashboard')
+    else:
+        return render_template('index.html')
+
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	if users.user_is_logged_in is not None:
-		return redirect('/dashboard')
+    """Login the user"""
+    if users.user_is_logged_in is not None:
+        return redirect('/dashboard')
 
-	data = {'host_url': request.host_url}
+    data = {}
 
-	if request.method == 'GET':
-		return render_template('login.html', data=data)
+    if request.method == 'GET':
+        return render_template('login.html', data=data)
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		email = request.form['email']
-		password = request.form['password']
+        email = request.form['email'].replace(" ", "")
+        password = request.form['password']
 
-		error = users.login(email, password)
+        error = users.login(email, password)
 
-		if error is not None:
-			data['error'] = str(error)
-			return render_template('login.html', data=data)
-		else:
-			return redirect('/dashboard')
+        if error is not None:
+            data['error'] = str(error)
+            return render_template('login.html', data=data)
+        else:
+            return redirect('/dashboard')
 
-	
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-	if users.user_is_logged_in is not None:
-		return redirect('/dashboard')
+    """Register a new user and login the user"""
+    if users.user_is_logged_in is not None:
+        return redirect('/dashboard')
 
-	data = {'host_url': request.host_url}
+    data = {}
 
-	if request.method == 'GET':
-		return render_template('register.html', data=data)
+    if request.method == 'GET':
+        return render_template('register.html', data=data)
 
-	if request.method == 'POST':
-		name = request.form['name']
-		email = request.form['email']
-		password = request.form['password']
-		confirm = request.form['confirm']
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email'].replace(" ", "")
+        password = request.form['password']
+        confirm = request.form['confirm']
 
-		error = users.register(name, email, password, confirm)
+        error = users.register(name, email, password, confirm)
 
-		if error is not None:
-			data['error'] = str(error)
-			return render_template('register.html', data=data)
-		else:
-			users.login(email, password)
-			return redirect('/dashboard')
+        if error is not None:
+            data['error'] = str(error)
+            return render_template('register.html', data=data)
+        else:
+            users.login(email, password)
+            return redirect('/dashboard')
+
 
 @app.route('/logout', methods=['GET'])
 def logout():
-	users.logout()
-	return redirect(url_for('index'))
+    """Logout the logged user"""
+    users.logout()
+    return redirect(url_for('index'))
 
-#display the buckets
+
 @app.route('/dashboard', methods=['GET'])
 def display_bucket():
-	if users.user_is_logged_in == None:
-		return redirect('/login')
+    """loop through the user's buckets and display the current buckets"""
+    if users.user_is_logged_in is None:
+        return redirect('/login')
 
-	data = dict()
-	data['host_url'] = request.host_url
-	data['user_bucketlists'] = []
-	logged_in_user = users.user_is_logged_in
+    data = dict()
+    data['host_url'] = request.host_url
+    data['user_bucketlists'] = []
+    logged_in_user = users.user_is_logged_in
 
-	if logged_in_user in bucketlists.keys():
-		user_bucketlists = bucketlists[logged_in_user]
-		data['user_bucketlists'] = user_bucketlists
-	return render_template('dashboard.html', data=data)
+    if logged_in_user in bucketlists.keys():
+        user_bucketlists = bucketlists[logged_in_user]
+        data['user_bucketlists'] = user_bucketlists
+    return render_template('dashboard.html', data=data)
+
+
 @app.route('/dashboard/add_bucket', methods=['POST'])
 def add_bucket():
-	bucket = request.form['bucket']
-	new_bucket = Buckets(bucket)
-	current_user = users.user_is_logged_in
+    """Add a new bucketlist for the current logged in user"""
+    bucket = request.form['bucket']
+    if len(bucket.replace(" ", "")) == 0:
+        msg = "The bucket name cannot be empty!"
+        return redirect('/dashboard')
+    else:
+        new_bucket = Buckets(bucket)
+        current_user = users.user_is_logged_in
 
-	if current_user in bucketlists.keys():
-		bucketlists[current_user].append(new_bucket)
-	else:
-		bucketlists[current_user] = [new_bucket]
-	return redirect('/dashboard')
+        if current_user in bucketlists.keys():
+            bucketlists[current_user].append(new_bucket)
+        else:
+            bucketlists[current_user] = [new_bucket]
+        return redirect('/dashboard')
+
 
 @app.route('/edit/<bucket_id>', methods=['POST', 'GET'])
 def edit_bucket(bucket_id):
-	if request.method == 'GET':
-		return render_template('edit_bucket.html', bucket_id=bucket_id)
+    """Edit a bucket associated with the bucket_id"""
+    if request.method == 'GET':
+        return render_template('edit_bucket.html', bucket_id=bucket_id)
 
-	if request.method == 'POST':
-		id = request.form['id']
-		new_title = request.form['new_title']
+    if request.method == 'POST':
+        id = request.form['id']
+        new_title = request.form['new_title']
 
-		
-		logged_in_user = users.user_is_logged_in
-		user_buckets = bucketlists[logged_in_user]
+        logged_in_user = users.user_is_logged_in
+        user_buckets = bucketlists[logged_in_user]
 
-		for bucket in user_buckets:
-			if str(bucket.id) == id:
-				bucket.update_bucket(new_title)
-				break
-		return redirect(url_for('display_bucket'))
-	
-
-
-	
+        for bucket in user_buckets:
+            if str(bucket.id) == id:
+                bucket.update_bucket(new_title)
+                break
+        return redirect(url_for('display_bucket'))
 
 
 @app.route('/dashboard/delete_bucket/<bucket_id>', methods=['GET', 'POST'])
 def delete_bucket(bucket_id):
-	logged_in_user = users.user_is_logged_in
+    """Delete a bucketlist from the user's bucketlists"""
+    logged_in_user = users.user_is_logged_in
 
-	if logged_in_user in bucketlists.keys():
-		user_buckets = bucketlists[logged_in_user]
+    if logged_in_user in bucketlists.keys():
+        user_buckets = bucketlists[logged_in_user]
 
-	count = 0
-	for bucket in user_buckets:
-		if str(bucket.id) == bucket_id:
-			user_buckets.pop(count)
-			break
+    count = 0
+    for bucket in user_buckets:
+        if str(bucket.id) == bucket_id:
+            user_buckets.pop(count)
+            break
 
-		count += 1
-	return redirect('/dashboard')
-	
+        count += 1
+    return redirect('/dashboard')
+
 
 @app.route('/dashboard/<bucket_id>', methods=["GET"])
 def view_bucket_activity(bucket_id):
-	data = {'host_url': request.host_url}
-	logged_in_user = users.user_is_logged_in
-	data['current_bucket'] = bucket_id
-	data['user_bucket'] = []
+    """Display the current activities under a selected bucket"""
+    data = {'host_url': request.host_url}
+    logged_in_user = users.user_is_logged_in
+    data['current_bucket'] = bucket_id
+    data['user_bucket'] = []
 
-	if logged_in_user in bucketlists.keys():
-		user_bucket = bucketlists[logged_in_user]
-		data['user_bucket'] = user_bucket
+    if logged_in_user in bucketlists.keys():
+        user_bucket = bucketlists[logged_in_user]
+        data['user_bucket'] = user_bucket
 
-	for bucket in user_bucket:
-		if str(bucket.id) == bucket_id:
-			data['user_activities'] = bucket.bucket_activities
-			break
-	return render_template('manage_bucket.html', data=data)
+    for bucket in user_bucket:
+        if str(bucket.id) == bucket_id:
+            data['user_activities'] = bucket.bucket_activities
+            break
+    return render_template('manage_bucket.html', data=data)
+
 
 @app.route('/manage/<bucket_id>/add_activity', methods=['POST'])
 def create_bucket_activity(bucket_id):
-	title = request.form['activity']
-	logged_in_user = users.user_is_logged_in
+    """Create a bucket activity under a selected bucketlist"""
+    title = request.form['activity']
+    if len(title.replace(" ", "")) == 0:
+        msg = "Activity title cannot be empty!"
+        return redirect('/dashboard/' + bucket_id)
+    else:
+        logged_in_user = users.user_is_logged_in
 
-	if logged_in_user in bucketlists.keys():
-		user_buckets = bucketlists[logged_in_user]
+        if logged_in_user in bucketlists.keys():
+            user_buckets = bucketlists[logged_in_user]
 
-	for bucket in user_buckets:
-		if str(bucket.id) == bucket_id:
-			bucket.create_activity(title)
-			flash('Bucket Added successfully')
-	return redirect('/dashboard/' + bucket_id)
+        for bucket in user_buckets:
+            if str(bucket.id) == bucket_id:
+                bucket.create_activity(title)
+                flash('Bucket Added successfully')
+        return redirect('/dashboard/' + bucket_id)
+
 
 @app.route('/edit_activity/<activity_id>', methods=['POST', 'GET'])
 def edit_activity(activity_id):
-	if request.method == 'GET':
-		return render_template('edit_activity.html', activity_id=activity_id)
+    """Edit an activity under a selected bucket"""
+    if request.method == 'GET':
+        return render_template('edit_activity.html', activity_id=activity_id)
 
-	if request.method == 'POST':
-		print('I see you')
-		id = request.form['id']
-		new_title = request.form['new_title']
-		print(new_title)
-		print(id)
+    if request.method == 'POST':
+        _id = request.form['id']
+        new_title = request.form['new_title']
 
-		
-		logged_in_user = users.user_is_logged_in
+        logged_in_user = users.user_is_logged_in
 
-		if logged_in_user in bucketlists.keys():
-			user_bucket = bucketlists[logged_in_user]
+        if logged_in_user in bucketlists.keys():
+            user_bucket = bucketlists[logged_in_user]
 
-		for bucket in user_bucket:
-			for item in bucket.bucket_activities:
-				if str(item['id']) == activity_id:
-					bucket.update_activity(id, new_title)
-					break
+        for bucket in user_bucket:
+            for item in bucket.bucket_activities:
+                if str(item['id']) == activity_id:
+                    print('three')
+                    bucket.update_activity(_id, new_title)
+                    break
 
-		
-		return redirect('/dashboard')
-		
+        return redirect('/dashboard')
+
 
 @app.route('/manage/<bucket_id>/delete/<activity_id>', methods=['GET', 'POST'])
 def delete_activity(bucket_id, activity_id):
-	logged_in_user = users.user_is_logged_in
+    """Delete a single bucketlist activity on the selected bucketlist"""
+    logged_in_user = users.user_is_logged_in
 
-	if logged_in_user in bucketlists.keys():
-		user_buckets = bucketlists[logged_in_user]
+    if logged_in_user in bucketlists.keys():
+        user_buckets = bucketlists[logged_in_user]
 
-	for bucket in user_buckets:
-		if str(bucket.id) == bucket_id:
-			for item in bucket.bucket_activities:
-				if str(item['id']) == str(activity_id):
-					bucket.delete_activity(activity_id)
-					break
-	return redirect('/dashboard/' + bucket_id)
+    for bucket in user_buckets:
+        if str(bucket.id) == bucket_id:
+            for item in bucket.bucket_activities:
+                if str(item['id']) == str(activity_id):
+                    bucket.delete_activity(activity_id)
+                    break
+    return redirect('/dashboard/' + bucket_id)
 
-app.secret_key = "thisiisasecret"
+app.secret_key = "secretkey"
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
