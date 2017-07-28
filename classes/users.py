dc8e2192d4bd78db20a4a85a12313da64aa6c91e
kolaempire <denokolas@gmail.com>
2017-07-28 08:00:40 +0300

    Check pep8

--- classes/users.py
+++ classes/users.py
@@ -3,14 +3,14 @@ class Users(object):
     def __init__(self):
         self.users = {}
         self.user_is_logged_in = None
-    
+
     def register(self, name, email, password, confirm):
         error_msg = None
 
-        if type(name) != str:
+        if type(name) is int:
             error_msg = "Name cannot contain numbers"
-        
-        if len(email) < 7: 
+
+        if len(email) < 7:
             error_msg = "Invalid email address."
 
         if '@' not in email:
@@ -37,29 +37,29 @@ class Users(object):
                     break
 
             if not user_exists:
-                #create account
+                # create account
                 self.users[email] = {'password': password, 'name': name}
 
         if error_msg is not None:
             return error_msg
-    
+
     def login(self, email, password):
         error_msg = None
 
-        if len(email) < 7: 
+        if len(email) < 7:
             error_msg = "Invalid email address."
 
         if '@' not in email:
             error_msg = "Invalid email address."
 
-        if (email[-4:] != '.com'):
+        if email[-4:] != '.com':
             error_msg = "Invalid email address."
 
         if len(password) == 0:
             error_msg = 'Invalid password'
         elif len(email) == 0:
             error_msg = 'email field cannot be left empty'
-        
+
         if error_msg is None:
             user_in_record = False
             for reg_email in self.users.keys():
@@ -76,9 +76,10 @@ class Users(object):
 
             if not user_in_record:
                 error_msg = 'User does not exist.'
-                
+
         if error_msg is not None:
             return error_msg
-    
+
     def logout(self):
+        """Logout the user"""
         self.user_is_logged_in = None
